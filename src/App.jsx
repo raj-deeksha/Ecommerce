17a9f7ab3f496a1a350945de6ef29e024325233f
raj-deeksha <rajdeeksha1999@gmail.com>
2024-05-15 15:18:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar/Navbar";
+import Slider from './ImageSlider/Slider'
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
